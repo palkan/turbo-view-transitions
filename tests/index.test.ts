9900dfd1a1862ae6fd628ff8237c1255ec5186fd
dkzlv <dkzlv@pm.me>
2023-10-12 10:37:24 +0100

    Adding most of the tooling required for work

---
+++ tests/index.test.ts
@@ -0,0 +1,32 @@
+import { suite } from "uvu";
+import * as assert from "uvu/assert";
+
+import { shouldPerformTransition } from "../src";
+import { reset, setup } from "./setup/env";
+
+setup();
+
+const shouldPeformTransition = suite("shouldPeformTransition");
+shouldPeformTransition.before.each(() => {
+  reset();
+  document.startViewTransition = (async () => {}) as unknown as any;
+});
+shouldPeformTransition("has meta", () => {
+  document.head.innerHTML = `
+    <meta name="view-transition" content="same-origin">
+  `;
+  assert.ok(shouldPerformTransition());
+});
+shouldPeformTransition("has no meta", () => {
+  assert.not.ok(shouldPerformTransition());
+});
+shouldPeformTransition("has meta but no startViewTransition", () => {
+  document.startViewTransition = undefined as unknown as any;
+
+  document.head.innerHTML = `
+    <meta name="view-transition" content="same-origin">
+  `;
+  assert.not.ok(shouldPerformTransition());
+});
+
+shouldPeformTransition.run();
---
+++ tests/setup/env.ts
@@ -0,0 +1,15 @@
+import { JSDOM } from "jsdom";
+
+export function setup() {
+  const dom = new JSDOM();
+  global.document = dom.window.document;
+  // @ts-expect-error Some APIs are missing, that's fine
+  global.window = dom.window;
+  global.navigator = dom.window.navigator;
+}
+
+export function reset() {
+  window.document.title = "";
+  window.document.head.innerHTML = "";
+  window.document.body.innerHTML = "<main></main>";
+}
